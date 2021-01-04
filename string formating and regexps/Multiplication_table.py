70d66a7daa20c03f057402bd9d10434532f1e24a
Roman <42977616+ivorobey@users.noreply.github.com>
2021-01-04 12:58:08 +0200

    x

--- string formating and regexps/Multiplication_table.py
+++ string formating and regexps/Multiplication_table.py
@@ -1,12 +1,10 @@
 
 def draw_table():
 
-    # for i in range(1, 11):
-    #     for j in range(1, 11):
-    #         print('{:>4}'.format(str(i*j)), end='')
-    #     print('\t')
+    for i in range(1, 5):
+        for j in range(1, 5):
+            print('{:>4}'.format(str(i*j)), end='')
+        print('\t')
 
-    multiples = [n for n in range(1, 15) for j in range(1, 15)]
-    return str(multiples)
 
 print(draw_table())
--- string formating and regexps/battery_capacity.py
+++ string formating and regexps/battery_capacity.py
@@ -1,20 +1,14 @@
-# import re
+# Task is to return the value of left battery capacity parsed from given string (let's assume it is some shell command's result).
 
-# data = '''
-#             "SuperMaxCapacity" =0
-#            "MaxCapacity" = 4540
-#            "CurrentCapacity" = 2897
-#            "LegacyBatteryInfo" = {"Amperage"=18446744073709550521,"Flags"=4,"Capacity"=4540,"Current"=2897,"Voltage"=7283,"Cycle Count"=406}
-#            "DesignCapacity" = 6700
-# '''
-# def get_battery_level(data): 
+# Current level should be calculated as CurrentCapacity / MaxCapacity in percents. The resulting value should be like the following example:
 
-#        max = int(re.search('"MaxCapacity" = (\d+)', data).group(1))
-#        cur = int(re.search('"CurrentCapacity" = (\d+)', data).group(1))
-#        result = float(float(cur) * 100 / float(max))
-#        return "{:.2f}%".format(result)
+# 61.41%
 
-# print(get_battery_level(data))
+# There are two ways:
+
+# Parse from LegacyBatteryInfo block - this is super easy. If you are beginner - try to get needed info from this line.
+# Parse from MaxCapacity/CurrentCapacity attributes - more complex task for more experienced programmers. If you want challenge - try to get information without using data from LegacyBatteryInfo block.
+# The result of the function should be in the form of the string: "XX.YY%" where XX.YY is the float number of percents with 2 digits after dot.
 
 import re
 data = '''
@@ -26,11 +20,8 @@ data = '''
 '''
 
 def get_battery_level(data):
-    regm=r'\bMaxCapacity....[0-9]+'
-    # regc=0
-    maxcap=re.findall(regm,data)
-    for i in maxcap:
-        print(i.split(','))
-    # curcap=re.findall(regc, data)
+    max_cap=re.search(r"\bMaxCapacity\D+(\d+)",data).group(1)
+    cur_cap=re.search(r"\bCurrentCapacity\D+(\d+)",data).group(1)
+    return "{:.2%}".format(int(cur_cap)/int(max_cap))
 
 print(get_battery_level(data))
